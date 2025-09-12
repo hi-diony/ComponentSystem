3407f8ef63874ea3d3e8fa3037b9a9d31879eb5c
jiyeonpark <jiyeonpark.1001@gmail.com>
2025-09-12 11:34:22 +0900

    build: 기본 프로젝트 구조 작업

---
+++ Examples/ComponentSystemExample/ComponentSystemExample/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ComponentSystemExample
+//
+//  Created by Station3 on 9/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
